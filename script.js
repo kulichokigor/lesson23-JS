f4f101ae5b155d9ca5b8df5acc33c9b4cb548c93
igorkulichok@gmail.com <igorkulichok@gmail.com>
2019-04-26 03:11:45 +0300

    homework

--- script.js
+++ script.js
@@ -16,3 +16,17 @@ var date = new Date(2013, 0, 25);
 var days = ['ндеділля', 'понеділок', 'вівторок', 'середа', 'четвер', 'п\'ятниця', 'субота'];
 var day = date.getDay();
 console.log(days[day]);
+
+//*Завдання 4
+var date = new Date();
+var age = new Date(prompt('введите свой день рождения', '1990-10-01'));
+var time = new Date(date.getFullYear(), age.getMonth(), age.getDate(), 0,0,0);
+var result = date.getTime() - time.getTime();
+result = Math.round(result / (1000 * 60 * 60 * 24));
+
+if(result < 0){
+	var birthDay = 365 + result;
+	alert('До Вашого дня народження залишилось: ' + birthDay + ' днів!')
+}else if(result > 0){
+	alert('До Вашого дня народження залишилось: ' + result + ' днів!')
+}else(alert('З днем народження!'));
